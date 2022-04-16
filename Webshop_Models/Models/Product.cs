937a7f816688f6a93e7434ede2a289c1b7f20d69
Arjan Torabi <arjan_t@live.nl>
2022-04-16 23:43:29 +0200

    adding files

---
+++ Webshop_Models/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Webshop_Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public bool IsActive { get; set; }
+        public double ? Price { get; set; } 
+        
+        public IEnumerable<ProductProp> ProductProperties { get; set; }
+
+    }
+}
